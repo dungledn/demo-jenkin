c2acb583692785c56ca9acd0a47c0bf062c099ba
Le Dung <dungleanh@DungLeAnh-Macbook.local>
2023-08-04 15:26:22 +0700

    Update convention

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,11 +10,13 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param Schedule $schedule schedule
+     *
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
+        dd($schedule);
         // $schedule->command('inspire')->hourly();
     }
 
